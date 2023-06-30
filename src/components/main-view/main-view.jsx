921953626316e11c1a0c5b405872aa73f93847cc
Alex Sweeney <ajsweeney2324@gmail.com>
2023-06-29 20:46:36 -0500

    adding first react views

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,11 @@
+export const MainView = () => {
+    return (
+        <div>
+            <div>Eloquent JavaScript</div>
+            <div>Mastering JavsScript Functional Programming</div>
+            <div>JavaScript: The Good Parts</div>
+            <div>JavaScript: The Definitive Guide</div>
+            <div>The Road to React</div>
+        </div>
+    )
+}
